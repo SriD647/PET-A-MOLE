dc9d921725d16a395ae259160f2c467725fdcc3b
sri416 <sridatt.rao@hotmail.com>
2023-02-16 18:57:38 -0500

    Finish Project 1 Game

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # PET-A-MOLE
+
+#**Welcome to PET-A-MOLE!**
+
+##**Game description**
+Your lawn has been swarmed by friendly moles and the only way to get them on their way is to show them some LOVE!. The game board is designed to look like a section of your lawn. It has has 9 evenly sized circles in the center, from which the moles will appear and disappear at random in less than a split second. The objective of the game is for the player to click (pet)  at least 12 moles before the 10 second countdown ends. A message will appear at the end of the round to indicate whether the player has won or lost. The game board also consists of a start button (to start a round), a reset button (to reset the game), a score board (to display current score), and a timer (to show time remaining in the round).
+
+The game is dervived from the original whack-a-mole, in which the players use a hammer to hit as many moles as possible in a round. However, this game has some controversy around it as the concept technically involves animal abuse. Hence, the game was modified by replacing the idea of hitting the animals to petting them.
+
+For best experience, aim the fingertips at the center of the mole's body. Google Chrome browser is recommended.
+
+##**Technologies used**
+HTML, CSS, and Javascript
+
+##**ICE Box items**
+* Different levels of difficulty
+* A pause button
+* Better styling and layout
+* Tighter and more efficient code
+* Sound effects
+
+##**Screenshots**
+![](https://imgur.com/letK6R9)
+![](https://imgur.com/IG1Ixr6)
+
--- main.js
+++ main.js
@@ -1,9 +1,90 @@
-console.log('java is workin');
+let circles=document.querySelectorAll('.location');
+const mole= document.getElementById('mole');
+const resetEl= document.getElementById('reset');
+const startEl=document.getElementById('start')
+const timeLeft= document.getElementById('timeLeft');
+const scoreCount=document.getElementById('score');
+let pointer=document.querySelector('location');
+let titleEl=document.getElementById('title');
 
-const cursor=document.querySelector('.cursor');
 
-window.addEventListener('mousemove', mouse=> {
-    cursor.style.top= mouse.pageY + 'px'
-    cursor.style.left=mouse.pageX + 'px'
+let score=0;
+let currentCircle=0;
+let pointLocked= false;
+let alreadyStart=false;
 
-})
+
+startEl.addEventListener('click', begin)
+
+/* Place mole in random location */
+function showMole () {
+    circles.forEach(function(circle) {
+      circle.classList.remove('mole');
+    });
+    
+    pointLocked=false;
+
+    let randomCircle= circles[Math.floor(Math.random()*9)];
+    randomCircle.classList.add('mole');
+
+    currentCircle=randomCircle.id;
+
+}
+
+/* Begin showMole function and countdown function */
+function begin() {
+    alreadyStart=true;
+    while (alreadyStart) {
+        startEl.removeEventListener ('click', begin);
+        score=0;
+        currentTime=10;
+        Timer=setInterval(showMole, 620);
+        timeTimer=setInterval(countDown, 1000);
+        timeTimer.innerText = `Countdown: ${0}`
+        scoreCount.innerText = `Moles petted: ${score}`
+        alreadyStart=false;
+    }    
+}
+
+/* Capture clicks and update scoreboard */
+circles.forEach(function (circle) {
+    circle.addEventListener('click', ()=>{
+    if(circle.id==currentCircle) {
+        if(pointLocked) return;
+        score++;
+        scoreCount.innerText= `Moles petted: ${score}`;
+        circle.classList.remove('mole');
+        pointLocked= true;
+    }
+    });
+});
+
+/*Count down timer and win/loss messages*/
+function countDown () {
+    currentTime--;
+    timeLeft.innerText= `Countdown: ${currentTime}`
+
+    if (currentTime== 0) {
+        startEl.addEventListener('click', begin)
+        currentTime=0;
+        clearInterval(timeTimer)
+        clearInterval(Timer)
+        if (score<12) {
+            titleEl.innerText ='Game over, you lost!';
+            startEl.removeEventListener ('click', begin);
+        }
+        if(score>=12) {
+            titleEl.innerText ='Game over, you won!';
+            startEl.removeEventListener ('click', begin);   
+        }
+    }
+
+}
+
+/*When reset button is clicked*/
+
+resetEl.addEventListener('click', reset)
+
+function reset() {
+    location.reload();
+}
